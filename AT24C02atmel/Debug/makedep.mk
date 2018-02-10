d15f344d07aafb8e5d686897a0ceee078b333f7e
G.Raf <sunriax@live.com>
2018-02-10 21:10:05 +0100

    Update
    Used STK500 port library for writing data to the LEDs of the
    STK500/Megacard

--- AT24C02atmel/Debug/makedep.mk
+++ AT24C02atmel/Debug/makedep.mk
@@ -2,6 +2,8 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
+..\..\STK500\port\port.c
+
 ..\..\STK500\twi\twi.c
 
 ..\AT24C02\at24c02.c
--- AT24C02atmel/main.c
+++ AT24C02atmel/main.c
@@ -14,22 +14,25 @@
  */
 
 #define F_CPU 12000000UL	// System clock
+#define OUTPUT 0b00000100	// Set output PORT (PORTC)
+
 #define ATMEL				// Define hardware platform
 
 //#define WRITE				// Define to Write Data to EEPROM
-#define ADDRESS 0x20
+#define ADDRESS 0x20		// Define EEPROM register address
 
 // Include standard libraries
 #include <util/delay.h>
 
 // Include EEPROM library
+#include "../../STK500/port/port.h"
 #include "../../STK500/twi/twi.h"
 #include "../AT24C02/at24c02.h"
 
 int main(void)
 {
-	DDRC = 0xFC;	// Setup DDRC (7:2) for verification
-					// Bit (1:0) are used for TWI bus!!!
+	port_init(OUTPUT, 0xFC, 0x00);	// Initialize PORT4(7:2) (PORTC) as OUTPUT
+									// Bit(1:0) is used by TWI bus
 	
 	at24c02_init();	// Initialize the EEPROM
 	
@@ -38,10 +41,12 @@ int main(void)
 		unsigned char string[] = "This is a secret";	// Save a string into an array
 		unsigned char data[] = { 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80, 0x90, 0xA0, 0xB0, 0xC0, 0xD0, 0xE0, 0xF0 };	// Save some values into an array
 		
-		PORTC = at24c02_write_string(ADDRESS, string, sizeof(string), 1);	// Write data string to EEPROM and verify data
-		//PORTC = at24c02_write_page(ADDRESS, data, sizeof(data), 1);		// Write a page to EEPROM and verify data
-		//PORTC = at24c02_write_byte(ADDRESS, 0x0A, 1);						// Write a single byte to EEPROM and verify data
+		unsigned char result = at24c02_write_string(ADDRESS, string, sizeof(string), 1);	// Write data string to EEPROM and verify data
+		//PORTC = at24c02_write_page(ADDRESS, data, sizeof(data), 1);						// Write a page to EEPROM and verify data
+		//PORTC = at24c02_write_byte(ADDRESS, 0x0A, 1);										// Write a single byte to EEPROM and verify data
 	
+		port_write(OUTPUT, (result & 0xFC), 0);		// Write result to PORT4 except bit (1:0)
+		
 		// Endless loop
 		while (1)
 		{
@@ -49,19 +54,19 @@ int main(void)
 	
 	#else
 	
-		unsigned char data[16];										// Buffer for reading data
+		unsigned char data[16];														// Buffer for reading data
+		unsigned char result = at24c02_read_string(ADDRESS, data, sizeof(data));	// Read data from EEPROM
 		
-		PORTC = at24c02_read_string(ADDRESS, data, sizeof(data));	// Read data from EEPROM
-		_delay_ms(2000);	// Wait some time that error can be red by user
+		port_write(OUTPUT, (result & 0xFC), 0);		// Write result to PORT4 except bit (1:0)
+		_delay_ms(2000);							// Wait some time that error can be red by user
 		
 		// Endless loop
 		while (1)
 		{
-			
 			// Display each byte onto PORTC
 			for(unsigned char i=0; i < sizeof(data); i++)
 			{
-				PORTC = data[i];	// Write data to PORTC 
+				port_write(OUTPUT, (data[i] & 0xFC), 0);		// Write data to PORT4 except bit (1:0)
 				_delay_ms(1000);	// Wait some time that user can verify that data that was written is valid
 			}
 		}	// End of loop
